c60436d6c345b7faa005958580e13e6a5cd341c5
JamesBilverstone98 <jdbilverstone@icloud.com>
2025-02-27 20:05:12 +0000

    Add files via upload

---
+++ age.py
@@ -0,0 +1,8 @@
+# AGE OF MATURITY = ask user for age, depending on age, print out a message
+
+age = int(input("How old are you? ")) # user input for age
+
+if age < 18:
+    print("You are not of age!")
+else:
+    print("You are of age!") # any input that is higher than 18 prints this
